6a4bed6504cf616c1c357522ce8eb037edf0e443
André Silveira <andre.silveira1silva@gmail.com>
2024-08-31 20:19:45 -0300

    feat: fetch user data from github api

--- src/pages/home/components/profile.tsx
+++ src/pages/home/components/profile.tsx
@@ -1,57 +1,79 @@
+import { useEffect, useState } from 'react'
 import {
   FaArrowUpRightFromSquare,
   FaBuilding,
   FaGithub,
   FaUserGroup,
 } from 'react-icons/fa6'
+import { NavLink } from 'react-router-dom'
+
+import { getProfile, GetProfileResponse } from '../../../api/get-profile'
 
 export function Header() {
+  const [userData, setUserData] = useState<GetProfileResponse>(
+    {} as GetProfileResponse,
+  )
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await getProfile()
+
+        setUserData(result)
+      } catch (erro) {
+        console.error(erro)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div>
-      <header className="flex items-center gap-8 rounded-[10px] bg-base-profile px-10 py-8">
-        <img
-          src="https://github.com/andresilveira1.png"
-          alt=""
-          className="h-36 w-36 rounded-lg"
-        />
+    <header className="flex items-center gap-8 rounded-[10px] bg-base-profile px-10 py-8">
+      <img
+        src={`https://github.com/${userData.login}.png`}
+        alt=""
+        className="h-36 w-36 rounded-lg"
+      />
 
-        <div>
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl font-bold text-base-title">
-              André Silveira Silva
-            </h1>
+      <div className="flex-1">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-bold text-base-title">
+            {userData?.name}
+          </h1>
 
-            <a
-              href="#"
-              className="flex items-center gap-2 text-xs font-bold text-base-blue"
-            >
-              GITHUB <FaArrowUpRightFromSquare />
-            </a>
-          </div>
-
-          <p className="line-clamp-2 text-base-text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa
-            dolores, reprehenderit minus facere aliquam libero qui, officiis,
-            nisi at tenetur sapiente. Sint minima consequuntur nulla nostrum,
-            voluptates quia iure ex.
-          </p>
-
-          <ul className="mt-6 flex space-x-6">
-            <li className="flex items-center gap-2">
-              <FaGithub className="h-5 w-5 text-base-label" />
-              <span className="text-base-subtitle">andresilveira1</span>
-            </li>
-            <li className="flex items-center gap-2">
-              <FaBuilding className="h-5 w-5 text-base-label" />
-              <span className="text-base-subtitle">FreeLancer</span>
-            </li>
-            <li className="flex items-center gap-2">
-              <FaUserGroup className="h-5 w-5 text-base-label" />
-              <span className="text-base-subtitle">10 seguidores</span>
-            </li>
-          </ul>
+          <NavLink
+            to={`https://github.com/${userData.login}`}
+            target="_blank"
+            className="flex items-center gap-2 text-xs font-bold text-base-blue"
+          >
+            GITHUB <FaArrowUpRightFromSquare />
+          </NavLink>
         </div>
-      </header>
-    </div>
+
+        <p className="line-clamp-2 text-base-text">
+          {userData?.bio ? userData.bio : 'Não informado'}
+        </p>
+
+        <ul className="mt-6 flex space-x-6">
+          <li className="flex items-center gap-2">
+            <FaGithub className="h-5 w-5 text-base-label" />
+            <span className="text-base-subtitle">{userData.login}</span>
+          </li>
+          <li className="flex items-center gap-2">
+            <FaBuilding className="h-5 w-5 text-base-label" />
+            <span className="text-base-subtitle">
+              {userData.company ? userData.company : 'Não informado'}
+            </span>
+          </li>
+          <li className="flex items-center gap-2">
+            <FaUserGroup className="h-5 w-5 text-base-label" />
+            <span className="text-base-subtitle">
+              {userData.followers} seguidores
+            </span>
+          </li>
+        </ul>
+      </div>
+    </header>
   )
 }
