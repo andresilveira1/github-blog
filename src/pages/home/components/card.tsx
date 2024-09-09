f6d5ae6e83ff0136ab16bc2f1f7070379f7ceeab
André Silveira <andre.silveira1silva@gmail.com>
2024-09-09 10:39:27 -0300

    feat: load data on card.

--- src/pages/home/components/card.tsx
+++ src/pages/home/components/card.tsx
@@ -1,22 +1,46 @@
-export default function Card() {
-  return (
-    <div className="h-[260px] w-[416px] space-y-5 rounded-lg bg-base-post p-8">
-      <div className="flex items-start">
-        <h2 className="w-[283px] text-xl font-bold text-base-title">
-          JavaScript data types and data structures
-        </h2>
-        <span className="text-sm text-base-span">Há 1 dia</span>
-      </div>
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { NavLink } from 'react-router-dom'
 
-      <p className="line-clamp-4 text-base-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quisquam
-        similique aut excepturi numquam quaerat maxime animi modi aliquam
-        molestias nobis rerum ducimus, odit, consequuntur ipsa vero quae?
-        Mollitia, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Cumque quisquam similique aut excepturi numquam quaerat maxime
-        animi modi aliquam molestias nobis rerum ducimus, odit, consequuntur
-        ipsa vero quae? Mollitia, ipsam.
-      </p>
-    </div>
+interface CardProps {
+  data:
+    | {
+        id: number
+        title: string
+        createdAt: string
+        description: string
+        number: number
+      }[]
+    | undefined
+}
+
+export function Card({ data }: CardProps) {
+  return (
+    <>
+      {data &&
+        data.map((issue) => {
+          return (
+            <NavLink
+              to={`details/${issue.number}`}
+              key={issue.id}
+              className="h-[260px] w-[416px] space-y-5 rounded-lg bg-base-post p-8"
+            >
+              <div className="flex items-start">
+                <h2 className="w-[283px] text-xl font-bold text-base-title">
+                  {issue.title}
+                </h2>
+                <span className="text-sm text-base-span">
+                  {formatDistanceToNow(issue.createdAt, {
+                    locale: ptBR,
+                    addSuffix: true,
+                  })}
+                </span>
+              </div>
+
+              <p className="line-clamp-4 text-base-text">{issue.description}</p>
+            </NavLink>
+          )
+        })}
+    </>
   )
 }
